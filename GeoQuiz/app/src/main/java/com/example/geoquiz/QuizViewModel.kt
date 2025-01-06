f7d06ccbdd38de7ef23b1e87d1f47b1bf2f12df7
Phillip <phillip.crawford3@gmail.com>
2025-01-05 23:27:03 -0700

    saveStateHandle

--- GeoQuiz/app/src/main/java/com/example/geoquiz/QuizViewModel.kt
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -1,12 +1,12 @@
 package com.example.geoquiz
 
 import android.util.Log
-import android.widget.Toast
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 private const val TAG = "QuizViewModel"
-
-class QuizViewModel : ViewModel() {
+const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
+class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
 
     init {
         Log.d(TAG, "ViewModel instance created")
@@ -26,7 +26,9 @@ class QuizViewModel : ViewModel() {
         Question(R.string.question_asia, true)
     )
 
-    private var currentIndex = 0
+    private var currentIndex: Int
+        get() = savedStateHandle.get(CURRENT_INDEX_KEY) ?: 0
+        set(value) = savedStateHandle.set(CURRENT_INDEX_KEY, value)
     private var correctAnswers = 0
     private var incorrectAnswers = 0
 
