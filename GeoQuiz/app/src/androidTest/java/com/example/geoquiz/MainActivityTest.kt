40bbe13c60f320628dca4865d7a53e6b8bb20012
Phillip <phillip.crawford3@gmail.com>
2025-01-09 16:39:04 -0700

    and another textview and button to layout xml file

--- GeoQuiz/app/src/androidTest/java/com/example/geoquiz/MainActivityTest.kt
+++ GeoQuiz/app/src/androidTest/java/com/example/geoquiz/MainActivityTest.kt
@@ -43,6 +43,14 @@ class MainActivityTest {
     }
 
     @Test
+    fun showsThirdQuestionAfterNextPress() {
+        onView(withId(R.id.next_button)).perform(click())
+        onView(withId(R.id.next_button)).perform(click())
+        onView(withId(R.id.question_text_view))
+            .check(matches(withText(R.string.question_mideast)))
+    }
+
+    @Test
     fun handlesActivityRecreation(){
         onView(withId(R.id.next_button)).perform(click())
         scenario.recreate()
