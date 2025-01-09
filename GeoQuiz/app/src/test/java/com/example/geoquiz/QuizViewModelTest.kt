85f4c82dd973faa772aa153502a49abe09cd66d5
Phillip <phillip.crawford3@gmail.com>
2025-01-08 17:06:48 -0700

    test passes

--- GeoQuiz/app/src/test/java/com/example/geoquiz/QuizViewModelTest.kt
+++ GeoQuiz/app/src/test/java/com/example/geoquiz/QuizViewModelTest.kt
@@ -8,6 +8,8 @@ import org.junit.Test
 class QuizViewModelTest {
     @Test
     fun providesExpectedQuestionText(){
-        
+        val savedStateHandle = SavedStateHandle()
+        val quizViewModel = QuizViewModel(savedStateHandle)
+        assertEquals(R.string.question_australia, quizViewModel.currentQuestionText)
     }
 }
