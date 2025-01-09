abbe391786bf72b55d74575f87a27dc03502393b
Phillip <phillip.crawford3@gmail.com>
2025-01-08 21:01:41 -0700

    add second passing test

--- GeoQuiz/app/src/androidTest/java/com/example/geoquiz/MainActivityTest.kt
+++ GeoQuiz/app/src/androidTest/java/com/example/geoquiz/MainActivityTest.kt
@@ -3,6 +3,7 @@ package com.example.geoquiz
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -32,5 +33,12 @@ class MainActivityTest {
         onView(withId(R.id.question_text_view))
             .check(matches(withText(R.string.question_australia)))
 
+    }
+
+    @Test
+    fun showsSecondQuestionAfterNextPress() {
+        onView(withId(R.id.next_button)).perform(click())
+        onView(withId(R.id.question_text_view))
+            .check(matches(withText(R.string.question_oceans)))
     }
 }
