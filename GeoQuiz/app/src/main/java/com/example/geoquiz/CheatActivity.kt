2621c576ff8970926bcbc8c923caf7a19735f153
Phillip <phillip.crawford3@gmail.com>
2025-01-09 17:25:24 -0700

    set binding in CheatActivity and add cheat button to activity_main.xml

--- GeoQuiz/app/src/main/java/com/example/geoquiz/CheatActivity.kt
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/CheatActivity.kt
@@ -5,9 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.geoquiz.databinding.ActivityCheatBinding
 
 class CheatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCheatBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
