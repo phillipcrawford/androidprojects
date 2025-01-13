bf6c6b01a81a5acd825c78820cece2b86e9eb1f8
Phillip <phillip.crawford3@gmail.com>
2025-01-12 20:24:42 -0700

    update target sdk

--- CriminaIntent/app/build.gradle.kts
+++ CriminaIntent/app/build.gradle.kts
@@ -5,12 +5,13 @@ plugins {
 
 android {
     namespace = "com.example.criminaintent"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.criminaintent"
         minSdk = 24
-        targetSdk = 34
+        //noinspection EditedTargetSdkVersion
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
