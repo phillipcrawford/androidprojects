1a5a06af76e44c1428c2207c922decc9e5d5cf08
Phillip <phillip.crawford3@gmail.com>
2025-01-13 19:41:09 -0700

    crime instance of a Crime now updates title to match contents of the EditText

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailFragment.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import com.example.criminaintent.databinding.FragmentCrimeDetailBinding
 import java.util.Date
@@ -37,5 +38,11 @@ class CrimeDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+        }
     }
 }
