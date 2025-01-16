7084af48584db97b21befe3ceb52c9fc46372825
Phillip <phillip.crawford3@gmail.com>
2025-01-16 09:14:12 -0700

    CrimeHolder Class

---
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.criminaintent
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.criminaintent.databinding.ListItemCrimeBinding
+
+class CrimeHolder (
+    val binding: ListItemCrimeBinding
+) : RecyclerView.ViewHolder(binding.root){
+
+}
+class CrimeListAdapter {
+}
