dbf492dfc7ba23c4eafd2d4f13e3b7c26cb9aab0
Phillip <phillip.crawford3@gmail.com>
2025-01-15 22:35:47 -0700

    must debug

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListFragment.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListFragment.kt
@@ -1,6 +1,19 @@
 package com.example.criminaintent
+import androidx.lifecycle.ViewModel
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
 
 private const val TAG = "CrimeListFragment"
 
-class CrimeListFragment {
+class CrimeListFragment : Fragment() {
+
+    private val crimeListViewModel: CrimeListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }
 }
