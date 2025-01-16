b48ef1bb2f57fca8f8e900e51461f405158d8045
Phillip <phillip.crawford3@gmail.com>
2025-01-16 08:54:20 -0700

    add nullable binding

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListFragment.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListFragment.kt
@@ -4,11 +4,17 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import android.util.Log
+import com.example.criminaintent.databinding.FragmentCrimeListBinding
 
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
+    private var _binding: FragmentCrimeListBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
 
@@ -16,4 +22,6 @@ class CrimeListFragment : Fragment() {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
     }
+
+    
 }
