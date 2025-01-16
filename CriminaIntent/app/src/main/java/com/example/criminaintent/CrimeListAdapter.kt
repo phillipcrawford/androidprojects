07c29d46f86fc619f32f4eecbf433bc4f420b31d
Phillip <phillip.crawford3@gmail.com>
2025-01-16 09:20:26 -0700

    CrimeListAdapter overrides three methods

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListAdapter.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListAdapter.kt
@@ -1,12 +1,35 @@
 package com.example.criminaintent
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.criminaintent.databinding.ListItemCrimeBinding
 
-class CrimeHolder (
+class CrimeHolder(
     val binding: ListItemCrimeBinding
 ) : RecyclerView.ViewHolder(binding.root){
 
 }
-class CrimeListAdapter {
+class CrimeListAdapter(
+    private val crimes: List<Crime>
+) : RecyclerView.Adapter<CrimeHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CrimeHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
+        return CrimeHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+        val crime = crimes[position]
+        holder.apply {
+            binding.crimeTitle.text = crime.title
+            binding.crimeDate.text = crime.date.toString()
+        }
+    }
+
+    override fun getItemCount() = crimes.size
 }
