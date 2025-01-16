83ed1dbeb296dff0d1d7ac29dd79322fe6021f36
Phillip <phillip.crawford3@gmail.com>
2025-01-15 22:36:06 -0700

    a

--- CriminaIntent/app/build.gradle.kts
+++ CriminaIntent/app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.constraintlayout)a
     implementation(libs.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
