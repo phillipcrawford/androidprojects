5f27326439c7a1bd2558c5001057645a5d6ad470
Phillip <phillip.crawford3@gmail.com>
2025-01-16 09:25:16 -0700

    add Toast to respond to presses

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListAdapter.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListAdapter.kt
@@ -2,6 +2,7 @@ package com.example.criminaintent
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.criminaintent.databinding.ListItemCrimeBinding
 
@@ -11,6 +12,14 @@ class CrimeHolder(
     fun bind(crime: Crime) {
         binding.crimeTitle.text = crime.title
         binding.crimeDate.text = crime.date.toString()
+
+        binding.root.setOnClickListener {
+            Toast.makeText(
+                binding.root.context,
+                "${crime.title} clicked!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
 class CrimeListAdapter(
