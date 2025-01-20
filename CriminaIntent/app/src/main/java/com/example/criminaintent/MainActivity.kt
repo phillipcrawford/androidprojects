3badfa1990a8492290434caf8622c20890612966
Phillip <phillip.crawford3@gmail.com>
2025-01-19 22:20:59 -0700

    enable edge to edge

--- CriminaIntent/app/src/main/java/com/example/criminaintent/MainActivity.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
     }
 }
