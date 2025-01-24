66a44547341a4870fff67ab345f2b5f1724f8086
Phillip <phillip.crawford3@gmail.com>
2025-01-23 22:31:04 -0700

    add TAGs in modelview

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListViewModel.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListViewModel.kt
@@ -1,17 +1,24 @@
 package com.example.criminaintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
+
+private const val TAG = "CrimeListViewModel"
 
 class CrimeListViewModel : ViewModel() {
 
     val crimes = mutableListOf<Crime>()
 
     init {
+        Log.d(TAG, "init starting")
         viewModelScope.launch {
+            Log.d(TAG, "coroutine launched")
+            delay(5000)
             for (i in 0 until 100) {
                 val crime = Crime(
                     id = UUID.randomUUID(),
@@ -22,6 +29,7 @@ class CrimeListViewModel : ViewModel() {
 
                 crimes += crime
             }
+            Log.d(TAG, "Loading crimes finished")
         }
     }
 }
