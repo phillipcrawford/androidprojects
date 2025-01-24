75503b7d7308ba53060aa32dd0e727f87e40a52e
Phillip <phillip.crawford3@gmail.com>
2025-01-23 22:23:40 -0700

    implement viewModelScope.launch in viewModel

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListViewModel.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.criminaintent
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
@@ -9,15 +11,17 @@ class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<Crime>()
 
     init {
-        for (i in 0 until 100){
-            val crime = Crime(
-                id = UUID.randomUUID(),
-                title = "Crime #$i",
-                date = Date(),
-                isSolved = i % 2 == 0
-            )
+        viewModelScope.launch {
+            for (i in 0 until 100) {
+                val crime = Crime(
+                    id = UUID.randomUUID(),
+                    title = "Crime #$i",
+                    date = Date(),
+                    isSolved = i % 2 == 0
+                )
 
-            crimes += crime
+                crimes += crime
+            }
         }
     }
 }
