f2bb531a39af2261d5c50e271f19f7661b35ed2e
Phillip <phillip.crawford3@gmail.com>
2025-01-23 23:33:03 -0700

    add ksp dependencies in gradle files

--- CriminaIntent/app/build.gradle.kts
+++ CriminaIntent/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -52,6 +53,9 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListFragment.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListFragment.kt
@@ -14,9 +14,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.criminaintent.databinding.FragmentCrimeListBinding
 import kotlinx.coroutines.launch
 
-
-private const val TAG = "CrimeListFragment"
-
 class CrimeListFragment : Fragment() {
     private var _binding: FragmentCrimeListBinding? = null
     private val binding
@@ -26,11 +23,6 @@ class CrimeListFragment : Fragment() {
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -39,10 +31,6 @@ class CrimeListFragment : Fragment() {
         _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
 
         binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-
-        val crimes = crimeListViewModel.crimes
-        val adapter = CrimeListAdapter(crimes)
-        binding.crimeRecyclerView.adapter = adapter
 
         return binding.root
     }
