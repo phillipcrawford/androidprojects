9f0b0681909602bfb84d6d0a3b729d666f431cff
Phillip <phillip.crawford3@gmail.com>
2025-01-23 22:40:51 -0700

    refactor into loadcrimes function, it's a suspended function, it can make use of other suspended functions

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListViewModel.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListViewModel.kt
@@ -18,18 +18,24 @@ class CrimeListViewModel : ViewModel() {
         Log.d(TAG, "init starting")
         viewModelScope.launch {
             Log.d(TAG, "coroutine launched")
-            delay(5000)
-            for (i in 0 until 100) {
-                val crime = Crime(
-                    id = UUID.randomUUID(),
-                    title = "Crime #$i",
-                    date = Date(),
-                    isSolved = i % 2 == 0
-                )
-
-                crimes += crime
-            }
+            crimes += loadCrimes()
             Log.d(TAG, "Loading crimes finished")
         }
+    }
+
+    suspend fun loadCrimes(): List<Crime> {
+        val result = mutableListOf<Crime>()
+        delay(5000)
+        for (i in 0 until 100) {
+            val crime = Crime(
+                id = UUID.randomUUID(),
+                title = "Crime #$i",
+                date = Date(),
+                isSolved = i % 2 == 0
+            )
+
+            result += crime
+        }
+        return result
     }
 }
