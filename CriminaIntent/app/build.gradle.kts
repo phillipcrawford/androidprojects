4e7a57753ccf5e215c5c6135ad4a02514fcd4625
Phillip <phillip.crawford3@gmail.com>
2025-01-24 23:58:29 -0700

    s

--- CriminaIntent/app/build.gradle.kts
+++ CriminaIntent/app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation(libs.lifecycle.runtime.ktx)
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
-    //ksp(libs.androidx.room.compiler)s
+    //ksp(libs.androidx.room.compiler)ss
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
