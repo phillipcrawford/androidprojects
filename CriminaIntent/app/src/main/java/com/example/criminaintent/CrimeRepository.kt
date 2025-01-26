a91282f354a184a8406d2cd5cc4992fb9f3fe650
Phillip <phillip.crawford3@gmail.com>
2025-01-26 04:18:46 -0700

    initialize CrimeRepository

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeRepository.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeRepository.kt
@@ -3,5 +3,13 @@ package com.example.criminaintent
 import android.content.Context
 
 class CrimeRepository private constructor(context: Context) {
-    
+    companion object {
+        private var INSTANCE: CrimeRepository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = CrimeRepository(context)
+            }
+        }
+    }
 }
