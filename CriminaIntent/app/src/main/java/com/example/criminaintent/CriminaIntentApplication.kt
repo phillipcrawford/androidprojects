5e714fcb8b82f40fb5848ceef21372849b7c9b69
Phillip <phillip.crawford3@gmail.com>
2025-01-26 04:32:01 -0700

    override onCreate

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CriminaIntentApplication.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CriminaIntentApplication.kt
@@ -3,5 +3,8 @@ package com.example.criminaintent
 import android.app.Application
 
 class CriminaIntentApplication : Application() {
-    
+    override fun onCreate() {
+        super.onCreate()
+        CrimeRepository.initialize(this)
+    }
 }
