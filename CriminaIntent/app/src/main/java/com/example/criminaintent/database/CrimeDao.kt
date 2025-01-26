9b54cf7a946fde3b48643b7652498efdb2782d64
Phillip <phillip.crawford3@gmail.com>
2025-01-25 22:58:14 -0700

    select one

--- CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeDao.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeDao.kt
@@ -8,4 +8,8 @@ import com.example.criminaintent.Crime
 interface CrimeDao {
     @Query("SELECT * FROM crime")
     suspend fun getCrimes(): List<Crime>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    suspend fun getCrime(id: UUID): Crime
+
 }
