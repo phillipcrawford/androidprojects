c31a5cf0b301a8071b43057688e77e2451fbec2a
Phillip <phillip.crawford3@gmail.com>
2025-01-25 20:11:56 -0700

    define parameters for Crimedatabase class

--- CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeDatabase.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeDatabase.kt
@@ -1,4 +1,12 @@
 package com.example.criminaintent.database
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.criminaintent.Crime
+
 class CrimeDatabase {
+    @Database(entities = [ Crime::class ], version = 1)
+    abstract class CrimeDatabase : RoomDatabase() {
+
+    }
 }
