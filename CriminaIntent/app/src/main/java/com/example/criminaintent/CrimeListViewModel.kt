decff9736d6a6315c5fa6de39696e3d82050c230
Phillip <phillip.crawford3@gmail.com>
2025-01-26 04:58:19 -0700

    private val crimeReRepository

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListViewModel.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListViewModel.kt
@@ -11,6 +11,7 @@ import java.util.UUID
 private const val TAG = "CrimeListViewModel"
 
 class CrimeListViewModel : ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
 
     val crimes = mutableListOf<Crime>()
 
