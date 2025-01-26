76aa7eccb263d13182fe7136d774aaafae7c25f4
Phillip <phillip.crawford3@gmail.com>
2025-01-25 20:34:34 -0700

    add first typeconverter

--- CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeTypeConverters.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeTypeConverters.kt
@@ -1,5 +1,11 @@
 package com.example.criminaintent.database
 
+import androidx.room.TypeConverter
+import java.util.Date
+
 class CrimeTypeConverters {
-    
+    @TypeConverter
+    fun fromDate(date: Date): Long {
+        return date.time
+    }
 }
