4fc27b10a25e56d7af9f8e732c892a87bab56a9f
Phillip <phillip.crawford3@gmail.com>
2025-01-25 20:37:55 -0700

    annotate CrimeDatabase to make use of CrimeTypeConverters

--- CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeDatabase.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeDatabase.kt
@@ -2,10 +2,12 @@ package com.example.criminaintent.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.criminaintent.Crime
 
 
 @Database(entities = [ Crime::class ], version = 1)
+@TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
 
 }
