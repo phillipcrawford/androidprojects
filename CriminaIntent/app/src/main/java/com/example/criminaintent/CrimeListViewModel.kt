cc9876d5b2b163a208c9f0b605d1aee037b47202
Phillip <phillip.crawford3@gmail.com>
2025-01-26 05:11:41 -0700

    flow -> stateflow

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListViewModel.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListViewModel.kt
@@ -3,6 +3,9 @@ package com.example.criminaintent
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeListViewModel"
@@ -10,7 +13,9 @@ private const val TAG = "CrimeListViewModel"
 class CrimeListViewModel : ViewModel() {
     private val crimeRepository = CrimeRepository.get()
 
-    val crimes = crimeRepository.getCrimes()
+    private val _crimes: MutableStateFlow<List<Crime>> = MutableStateFlow(emptyList())
+    val crimes: StateFlow<List<Crime>>
+        get() = _crimes.asStateFlow()
 
     init {
         viewModelScope.launch {
