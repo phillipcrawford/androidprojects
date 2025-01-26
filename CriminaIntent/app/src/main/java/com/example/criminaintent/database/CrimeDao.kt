f03ca5cea7058a0ef0ab98018d7ace237219de5c
Phillip <phillip.crawford3@gmail.com>
2025-01-25 22:58:38 -0700

    import UUID

--- CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeDao.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeDao.kt
@@ -3,6 +3,7 @@ package com.example.criminaintent.database
 import androidx.room.Dao
 import androidx.room.Query
 import com.example.criminaintent.Crime
+import java.util.UUID
 
 @Dao
 interface CrimeDao {
