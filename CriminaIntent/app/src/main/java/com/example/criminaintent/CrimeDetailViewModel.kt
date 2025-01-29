d80d7123264d083103afe4908e0c4ba35fd67c20
Phillip <phillip.crawford3@gmail.com>
2025-01-28 23:48:04 -0700

    get crimerepo, nullable crime MutableStateFlow, non-nullable StateFlow

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailViewModel.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.criminaintent
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
 class CrimeDetailViewModel {
+    private val crimeRepository = CrimeRepository.get()
+
+    private val _crime: MutableStateFlow<Crime?> = MutableStateFlow(null)
+    val crime: StateFlow<Crime?> = _crime.asStateFlow()
 }
