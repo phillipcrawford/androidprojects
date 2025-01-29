897ba36b3878c9a7b300e03da7d5511647c5b71c
Phillip <phillip.crawford3@gmail.com>
2025-01-28 22:54:56 -0700

    delete references to crime

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailFragment.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailFragment.kt
@@ -9,10 +9,6 @@ import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.criminaintent.databinding.FragmentCrimeDetailBinding
-import java.util.Date
-import java.util.UUID
-
-private const val TAG = "CrimeDetailFragment"
 
 class CrimeDetailFragment : Fragment() {
 
@@ -22,22 +18,7 @@ class CrimeDetailFragment : Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
-    private lateinit var crime: Crime
-
     private val args: CrimeDetailFragmentArgs by navArgs()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        crime = Crime(
-            id = UUID.randomUUID(),
-            title = "",
-            date = Date(),
-            isSolved = false
-        )
-
-        Log.d(TAG, "The crime ID is: ${args.crimeId}")
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,16 +34,13 @@ class CrimeDetailFragment : Fragment() {
 
         binding.apply {
             crimeTitle.doOnTextChanged { text, _, _, _ ->
-                crime = crime.copy(title = text.toString())
             }
 
             crimeDate.apply {
-                text = crime.date.toString()
                 isEnabled = false
             }
 
             crimeSolved.setOnCheckedChangeListener { _, isChecked ->
-                crime = crime.copy(isSolved = isChecked)
             }
         }
     }
