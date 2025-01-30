d9a1ac029aa0b6e2c426c099bf8cda4d594abccc
Phillip <phillip.crawford3@gmail.com>
2025-01-29 22:04:01 -0700

    imports

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailViewModel.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailViewModel.kt
@@ -1,16 +1,20 @@
 package com.example.criminaintent
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.util.UUID
 
-class CrimeDetailViewModel(crimeId: UUID) {
+class CrimeDetailViewModel(crimeId: UUID) : ViewModel() {
     private val crimeRepository = CrimeRepository.get()
 
     private val _crime: MutableStateFlow<Crime?> = MutableStateFlow(null)
     val crime: StateFlow<Crime?> = _crime.asStateFlow()
 
-    init{
+    init {
         viewModelScope.launch {
             _crime.value = crimeRepository.getCrime(crimeId)
         }
