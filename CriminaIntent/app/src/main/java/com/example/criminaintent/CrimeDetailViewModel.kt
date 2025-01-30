7979b04d887d503d09180faa8bc1965661fdd00b
Phillip <phillip.crawford3@gmail.com>
2025-01-30 01:09:52 -0700

    remove comment

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailViewModel.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailViewModel.kt
@@ -30,9 +30,7 @@ class CrimeDetailViewModel(crimeId: UUID) : ViewModel() {
 
     override fun onCleared() {
         super.onCleared()
-        //viewModelScope.launch {
-            crime.value?.let { crimeRepository.updateCrime(it) }
-        //}
+        crime.value?.let { crimeRepository.updateCrime(it) }
     }
 }
 
