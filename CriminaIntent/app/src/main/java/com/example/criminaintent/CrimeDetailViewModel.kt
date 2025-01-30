0bed75b64eab04fde8505cdc12b58679fb284e13
Phillip <phillip.crawford3@gmail.com>
2025-01-29 22:45:38 -0700

    update crime ifun in viewmodel

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailViewModel.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.util.UUID
 
@@ -18,6 +19,12 @@ class CrimeDetailViewModel(crimeId: UUID) : ViewModel() {
     init {
         viewModelScope.launch {
             _crime.value = crimeRepository.getCrime(crimeId)
+        }
+    }
+
+    fun updateCrime(onUpdate: (Crime) -> Crime) {
+        _crime.update { oldCrime ->
+            oldCrime?.let { onUpdate(it) }
         }
     }
 }
