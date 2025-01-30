1e375458c06921de61bc66d031808b6ee8a9e20d
Phillip <phillip.crawford3@gmail.com>
2025-01-29 22:59:23 -0700

    update DAO and Repository

--- CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeDao.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeDao.kt
@@ -2,6 +2,7 @@ package com.example.criminaintent.database
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Update
 import com.example.criminaintent.Crime
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
@@ -14,4 +15,6 @@ interface CrimeDao {
     @Query("SELECT * FROM crime WHERE id=(:id)")
     suspend fun getCrime(id: UUID): Crime
 
+    @Update
+    suspend fun updateCrime(crime: Crime)
 }
