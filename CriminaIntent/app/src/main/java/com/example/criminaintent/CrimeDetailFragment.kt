28c74cebe8a6e05ace9dc06ab252699bf7c451f7
Phillip <phillip.crawford3@gmail.com>
2025-01-30 18:26:10 -0700

    revert

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailFragment.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailFragment.kt
@@ -34,7 +34,7 @@ class CrimeDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCrimeDetailBinding.inflate(inflater, container, false)
-        return binding.rootabcde
+        return binding.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
