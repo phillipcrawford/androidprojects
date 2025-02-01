68d8c9abd3e6a34f0e03ce798b0f3737ea1b1477
Phillip <phillip.crawford3@gmail.com>
2025-01-31 23:23:57 -0700

    return statement

--- CriminaIntent/app/src/main/java/com/example/criminaintent/DatePickerFragment.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/DatePickerFragment.kt
@@ -12,6 +12,13 @@ class DatePickerFragment : DialogFragment() {
         val initialYear = calendar.get(Calendar.YEAR)
         val initialMonth = calendar.get(Calendar.MONTH)
         val initialDay = calendar.get(Calendar.DAY_OF_MONTH)
-        
+
+        return DatePickerDialog(
+            requireContext(),
+            null,
+            initialYear,
+            initialMonth,
+            initialDay
+        )
     }
 }
