cf7ce5f561c0d218ee7dca9f743d9f1eaca0f472
Phillip <phillip.crawford3@gmail.com>
2025-02-02 02:29:10 -0700

    delete import

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListFragment.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeListFragment.kt
@@ -3,7 +3,6 @@ package com.example.criminaintent
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
