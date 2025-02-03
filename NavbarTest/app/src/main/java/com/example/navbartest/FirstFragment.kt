4e9407074a58441c813e4f9cdd016811adae2bdc
Phillip <phillip.crawford3@gmail.com>
2025-02-03 06:38:06 -0700

    new strings

--- NavbarTest/app/src/main/java/com/example/navbartest/FirstFragment.kt
+++ NavbarTest/app/src/main/java/com/example/navbartest/FirstFragment.kt
@@ -7,9 +7,11 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import com.example.navbartest.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
@@ -28,7 +30,28 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.fragment_first, menu)
+            }
 
+            /**
+             * Responding to the user selecting an item in the menu, in this case
+             * adding a new crime. Several menu items can be added here.
+             */
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection
+                return when (menuItem.itemId) {
+                    R.id.plus_sign -> {
+                        Toast.makeText(context, "New Trial!", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 //        super.onViewCreated(view, savedInstanceState)
 //
