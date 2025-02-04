906b5a6b9d654ae557cc32091381ab90a0593fef
Phillip <phillip.crawford3@gmail.com>
2025-02-04 07:49:58 -0700

    call addmigration in crimerepositiory

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeRepository.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeRepository.kt
@@ -3,6 +3,7 @@ package com.example.criminaintent
 import android.content.Context
 import androidx.room.Room
 import com.example.criminaintent.database.CrimeDatabase
+import com.example.criminaintent.database.migration_1_2
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
@@ -23,6 +24,7 @@ class CrimeRepository private constructor(
             DATABASE_NAME
         )
         .createFromAsset(DATABASE_NAME)
+        .addMigrations(migration_1_2)
         .build()
 
     fun getCrimes(): Flow<List<Crime>> = database.crimeDao().getCrimes()
