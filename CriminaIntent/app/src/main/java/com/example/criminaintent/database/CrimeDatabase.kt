86daf2f906c0ae38558a447f5d7e5014dd290d44
Phillip <phillip.crawford3@gmail.com>
2025-02-04 07:45:09 -0700

    import

--- CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeDatabase.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/database/CrimeDatabase.kt
@@ -3,6 +3,7 @@ package com.example.criminaintent.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.criminaintent.Crime
 
