b0c621b5ad2cdc8a3fabc27aeb6f585d3181214f
Phillip <phillip.crawford3@gmail.com>
2025-02-05 06:32:57 -0700

    URI

--- CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailFragment.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/CrimeDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.example.criminaintent
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.format.DateFormat
 import android.view.LayoutInflater
