55576ecedbd0acfc21ee48ff3ed2c8a8bbdbeb15
Phillip <phillip.crawford3@gmail.com>
2025-02-08 15:25:31 -0700

    options

--- CriminaIntent/app/src/main/java/com/example/criminaintent/getScaledBitmap.kt
+++ CriminaIntent/app/src/main/java/com/example/criminaintent/getScaledBitmap.kt
@@ -1,5 +1,10 @@
 package com.example.criminaintent
 
-fun getScaledBitmap(path: String, destWidth: Int, destHeight: Int): Bitmap {
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 
+fun getScaledBitmap(path: String, destWidth: Int, destHeight: Int): Bitmap {
+    // Read in the dimensions of the image on disk
+    val options = BitmapFactory.Options()
+    options.inJustDecodeBounds = true
 }
