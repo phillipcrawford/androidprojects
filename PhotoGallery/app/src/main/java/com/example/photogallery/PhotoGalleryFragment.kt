aa18a86bf950ef33d4978b7d6cd4d05d51d74268
Phillip <phillip.crawford3@gmail.com>
2025-02-12 18:46:56 -0700

    nullable and non nullable binding

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -1,9 +1,14 @@
 package com.example.photogallery
 
 import androidx.fragment.app.Fragment
+import com.example.photogallery.databinding.FragmentPhotoGalleryBinding
 
 class PhotoGalleryFragment {
     class PhotoGalleryFragment : Fragment(){
-        
+        private var _binding: FragmentPhotoGalleryBinding? = null
+        private var binding
+            get() = checkNotNull(_binding) {
+                "Cannot access binding because it is null. Is the view visible?"
+            }
     }
 }
