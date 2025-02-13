b3065b854caa80e3e838ecce9b5788f567439ade
Phillip <phillip.crawford3@gmail.com>
2025-02-13 10:47:34 -0700

    coroutines android

--- PhotoGallery/app/build.gradle.kts
+++ PhotoGallery/app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
