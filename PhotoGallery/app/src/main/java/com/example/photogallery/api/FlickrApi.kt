da3cb611b43415cd61a123a7d12fcdafe271455d
Phillip <phillip.crawford3@gmail.com>
2025-02-15 20:02:04 -0700

    bring url string in

--- PhotoGallery/app/src/main/java/com/example/photogallery/api/FlickrApi.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/api/FlickrApi.kt
@@ -4,7 +4,11 @@ import retrofit2.http.GET
 
 interface FlickrApi {
     @GET(
-        
+        "services/rest/?method=flickr.interestingness.getList" +
+            "&api_key=f59142b24d41487d124b33c63a65bf2f" +
+            "&format=json" +
+            "&nojsoncallback=1" +
+            "&extras=url_s"
     )
     suspend fun fetchPhotos():String
 }
