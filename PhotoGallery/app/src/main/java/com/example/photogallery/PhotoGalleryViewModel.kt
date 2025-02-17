9a52702208af252300ad49aba7cbcd800091204b
Phillip <phillip.crawford3@gmail.com>
2025-02-17 14:53:02 -0700

    TAG

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
@@ -2,6 +2,8 @@ package com.example.photogallery
 
 import androidx.lifecycle.ViewModel
 
+private const val TAG = "PhotoGalleryViewModel"
+
 class PhotoGalleryViewModel : ViewModel {
     private val photoRepository = PhotoRepository()
 }
