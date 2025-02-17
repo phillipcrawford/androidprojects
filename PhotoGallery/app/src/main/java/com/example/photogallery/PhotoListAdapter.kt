83d6434453ddbf2ec63438f4cc0534d4ea3f848c
Phillip <phillip.crawford3@gmail.com>
2025-02-17 16:49:04 -0700

    PhotoViewHolder &PhotoListAdapter

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoListAdapter.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoListAdapter.kt
@@ -1,13 +1,25 @@
 package com.example.photogallery
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.photogallery.api.GalleryItem
 import com.example.photogallery.databinding.ListItemGalleryBinding
 
-class PhotoListAdapter (
+class PhotoViewHolder (
     private val binding: ListItemGalleryBinding
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(galleryItem: GalleryItem) {
         //TODO
+    }
+}
+
+class PhotoListAdapter(
+    private val galleryItems: List<GalleryItem>
+) : RecyclerView.Adapter<PhotoViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): PhotoViewHolder {
+
     }
 }
