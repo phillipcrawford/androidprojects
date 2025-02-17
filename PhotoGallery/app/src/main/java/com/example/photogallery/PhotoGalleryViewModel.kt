8412e084237e81410aa05e23aa739ff98b32afde
Phillip <phillip.crawford3@gmail.com>
2025-02-17 15:02:50 -0700

    body

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
@@ -1,9 +1,18 @@
 package com.example.photogallery
 
 import androidx.lifecycle.ViewModel
+import com.example.photogallery.api.GalleryItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 private const val TAG = "PhotoGalleryViewModel"
 
-class PhotoGalleryViewModel : ViewModel {
+class PhotoGalleryViewModel : ViewModel() {
     private val photoRepository = PhotoRepository()
+
+    private val _galleryItems: MutableStateFlow<List<GalleryItem>> =
+        MutableStateFlow(emptyList())
+    val galleryItems: StateFlow<List<GalleryItem>>
+        get() = _galleryItems.asStateFlow()
 }
