52f74330682801cbf308f050e33710c2e9300276
Phillip <phillip.crawford3@gmail.com>
2025-02-17 15:36:35 -0700

    instance of PhotoGalleryViewModel

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.photogallery.databinding.FragmentPhotoGalleryBinding
@@ -31,6 +32,8 @@ class PhotoGalleryFragment : Fragment(){
         return binding.root
     }
 
+    private val photoGalleryViewModel: PhotoGalleryViewModel by viewModels()
+    
         override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
             super.onViewCreated(view, savedInstanceState)
 
