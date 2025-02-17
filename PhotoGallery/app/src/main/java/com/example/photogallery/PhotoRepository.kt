78cb0d804fc907fbb3442aa0ed46e10e9c24d765
Phillip <phillip.crawford3@gmail.com>
2025-02-16 19:45:33 -0700

    body matches retrun type

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoRepository.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoRepository.kt
@@ -1,6 +1,7 @@
 package com.example.photogallery
 
 import com.example.photogallery.api.FlickrApi
+import com.example.photogallery.api.GalleryItem
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -17,6 +18,6 @@ class PhotoRepository {
     }
 
     suspend fun fetchPhotos(): List<GalleryItem> =
-        flickrApi.fetchPhotos()
+        flickrApi.fetchPhotos().photos.galleryItems
 
 }
