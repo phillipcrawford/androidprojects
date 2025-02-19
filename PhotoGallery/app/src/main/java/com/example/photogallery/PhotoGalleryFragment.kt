8e745d8ec0d1b9934cd5591b0fc8aa66a03b7264
Phillip <phillip.crawford3@gmail.com>
2025-02-18 19:58:32 -0700

    i

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -14,7 +14,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.example.photogallery.databinding.FragmentPhotoGalleryBinding
 import kotlinx.coroutines.launch
 
-private const val TAG = "PhotoGalleryFragmentabcdefgh"
+private const val TAG = "PhotoGalleryFragmentabcdefghi"
 
 class PhotoGalleryFragment : Fragment() {
     private var _binding: FragmentPhotoGalleryBinding? = null
