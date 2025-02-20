328a611919af3604a9eef240b7c7f6cc97573ba0
Phillip <phillip.crawford3@gmail.com>
2025-02-19 19:16:05 -0700

    revert

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -20,7 +20,7 @@ class PhotoGalleryFragment : Fragment() {
     private var _binding: FragmentPhotoGalleryBinding? = null
     private val binding
         get() = checkNotNull(_binding) {
-            "Cannot access binding because it is null. Is the view visiblabcdefghe?"
+            "Cannot access binding because it is null. Is the view visible?"
         }
 
     override fun onCreateView(
