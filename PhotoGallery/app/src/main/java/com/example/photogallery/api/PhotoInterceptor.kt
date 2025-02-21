645599104e935bb0cfc72510c9601a7baaac274d
Phillip <phillip.crawford3@gmail.com>
2025-02-20 22:05:35 -0700

    import Response

--- PhotoGallery/app/src/main/java/com/example/photogallery/api/PhotoInterceptor.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/api/PhotoInterceptor.kt
@@ -1,11 +1,12 @@
 package com.example.photogallery.api
 
 import okhttp3.Interceptor
+import okhttp3.Response
 
 private const val API_KEY = "f59142b24d41487d124b33c63a65bf2f"
 
 class PhotoInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        
+
     }
 }
