2e30d732f4bb5846b15a004010d102cfef1b490e
Phillip <phillip.crawford3@gmail.com>
2025-02-20 22:10:33 -0700

    newUrl

--- PhotoGallery/app/src/main/java/com/example/photogallery/api/PhotoInterceptor.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/api/PhotoInterceptor.kt
@@ -1,5 +1,6 @@
 package com.example.photogallery.api
 
+import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
@@ -9,5 +10,9 @@ private const val API_KEY = "f59142b24d41487d124b33c63a65bf2f"
 class PhotoInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest: Request = chain.request()
+
+        val newUrl: HttpUrl = originalRequest.url.newBuilder()
+            .addQueryParameter("api_key", API_KEY)
+            .addQueryParameter("format", "json")
     }
 }
