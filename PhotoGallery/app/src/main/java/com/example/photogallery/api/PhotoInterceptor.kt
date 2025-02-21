1e25a7a6ab2a0417b91e86d718039b69a70e8d7c
Phillip <phillip.crawford3@gmail.com>
2025-02-20 22:05:16 -0700

    import and override

--- PhotoGallery/app/src/main/java/com/example/photogallery/api/PhotoInterceptor.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/api/PhotoInterceptor.kt
@@ -1,6 +1,11 @@
 package com.example.photogallery.api
 
+import okhttp3.Interceptor
+
 private const val API_KEY = "f59142b24d41487d124b33c63a65bf2f"
 
 class PhotoInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        
+    }
 }
