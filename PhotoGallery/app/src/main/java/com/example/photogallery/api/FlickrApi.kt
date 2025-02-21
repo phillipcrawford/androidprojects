5bdb844510ace0144d1fa02b3dfa26cbd7722b0c
Phillip <phillip.crawford3@gmail.com>
2025-02-21 15:50:43 -0700

    @GET

--- PhotoGallery/app/src/main/java/com/example/photogallery/api/FlickrApi.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/api/FlickrApi.kt
@@ -1,6 +1,7 @@
 package com.example.photogallery.api
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface FlickrApi {
 //    @GET(
@@ -12,4 +13,7 @@ interface FlickrApi {
 //    )
     @GET("services/rest/?method=flickr.interestingness.getList")
     suspend fun fetchPhotos(): FlickrResponse
+
+    @GET("services/rest?method=flickr.photos.search")
+    suspend fun searchPhotos(@Query("text") query: String): FlickrResponse
 }
