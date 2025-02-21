c487858320bf27e3d151878505ac1a191652b618
Phillip <phillip.crawford3@gmail.com>
2025-02-20 22:07:20 -0700

    originalRequest value of type Request will equal chain parameter's request() method

--- PhotoGallery/app/src/main/java/com/example/photogallery/api/PhotoInterceptor.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/api/PhotoInterceptor.kt
@@ -7,6 +7,6 @@ private const val API_KEY = "f59142b24d41487d124b33c63a65bf2f"
 
 class PhotoInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-
+        val originalRequest: Request = chain.request()
     }
 }
