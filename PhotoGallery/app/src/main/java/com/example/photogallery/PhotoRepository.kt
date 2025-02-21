ae58accae81c640745150c51a7ed8fc377af18f6
Phillip <phillip.crawford3@gmail.com>
2025-02-21 15:45:17 -0700

    add an interceptor to Retrofit configuration

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoRepository.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoRepository.kt
@@ -2,6 +2,8 @@ package com.example.photogallery
 
 import com.example.photogallery.api.FlickrApi
 import com.example.photogallery.api.GalleryItem
+import com.example.photogallery.api.PhotoInterceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -9,9 +11,14 @@ class PhotoRepository {
     private val flickrApi: FlickrApi
 
     init {
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(PhotoInterceptor())
+            .build()
+
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://api.flickr.com/")
             .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
             .build()
 
         flickrApi = retrofit.create(FlickrApi::class.java)
