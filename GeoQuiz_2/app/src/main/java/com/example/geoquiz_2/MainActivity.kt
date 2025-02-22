66d690df9034fc22a7e8bc45836755eda0737274
Phillip <phillip.crawford3@gmail.com>
2025-02-22 15:29:12 -0700

    move questionbank & current index into viewModel, helper functions to navigate

--- GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/MainActivity.kt
+++ GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/MainActivity.kt
@@ -19,17 +19,6 @@ class MainActivity : AppCompatActivity() {
 
     private val quizViewModel: QuizViewModel by viewModels()
 
-    private var currentIndex = 0
-
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
