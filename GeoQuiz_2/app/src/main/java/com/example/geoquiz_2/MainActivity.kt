b48cd7728d7b1bb0fa4cdad8d33362c76b9cf3a7
Phillip <phillip.crawford3@gmail.com>
2025-02-21 19:44:13 -0700

    complete chapter 1

--- GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/MainActivity.kt
+++ GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.geoquiz_2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
+        falseButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
