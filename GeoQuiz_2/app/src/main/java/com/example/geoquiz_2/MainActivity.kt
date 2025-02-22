9417905b262e6ab4cee769eb638ccf6b24bd6579
Phillip <phillip.crawford3@gmail.com>
2025-02-21 20:26:42 -0700

    page 39

--- GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/MainActivity.kt
+++ GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/MainActivity.kt
@@ -13,6 +13,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
 
+    private var currentIndex = 0
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
