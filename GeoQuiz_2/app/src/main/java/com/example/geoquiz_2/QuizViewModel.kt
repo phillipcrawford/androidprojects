44077a328deb244fc9840c20553afe7890667cd0
Phillip <phillip.crawford3@gmail.com>
2025-02-22 13:59:50 -0700

    boilerplate viewModel

---
+++ GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/QuizViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.geoquiz_2
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel : ViewModel() {
+
+    init {
+        Log.d(TAG, "ViewModel instance created")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d(TAG, "ViewModel instance about to be destroyed")
+    }
+}
