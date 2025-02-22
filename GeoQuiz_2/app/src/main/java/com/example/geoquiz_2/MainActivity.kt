af429c0bc6c8cf174e478e8cbe358abd5fff0bb7
Phillip <phillip.crawford3@gmail.com>
2025-02-21 21:55:49 -0700

    check answer helper function

--- GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/MainActivity.kt
+++ GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/MainActivity.kt
@@ -7,11 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.geoquiz_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
 
     private var currentIndex = 0
 
@@ -27,16 +27,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener {
+        binding.trueButton.setOnClickListener {
             Toast.makeText(
                 this,
                 R.string.correct_toast,
@@ -44,12 +43,37 @@ class MainActivity : AppCompatActivity() {
             ).show()
 
         }
-        falseButton.setOnClickListener {
+
+        binding.falseButton.setOnClickListener {
             Toast.makeText(
                 this,
                 R.string.incorrect_toast,
                 Toast.LENGTH_SHORT
             ).show()
         }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+                R.string.correct_toast
+            } else {
+                R.string.incorrect_toast
+            }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 }
