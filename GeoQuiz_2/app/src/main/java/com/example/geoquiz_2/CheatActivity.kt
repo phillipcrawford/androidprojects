92f1939c0d0a18ba02d5d186d23cd3de9d9bbf3e
Phillip <phillip.crawford3@gmail.com>
2025-02-23 18:14:07 -0700

    using bindings to inflate xml in corresponding kotlin classfile

--- GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/CheatActivity.kt
+++ GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/CheatActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.geoquiz_2.databinding.ActivityCheatBinding
 
 class CheatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCheatBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cheat)
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
