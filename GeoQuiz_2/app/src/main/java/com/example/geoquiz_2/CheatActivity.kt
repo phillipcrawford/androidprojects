e1b3142b9092f0f7e411b1d1307dd88a1bd9f214
Phillip <phillip.crawford3@gmail.com>
2025-02-23 20:23:46 -0700

    call intent.getbooleanExtra to get answer out of intent

--- GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/CheatActivity.kt
+++ GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/CheatActivity.kt
@@ -14,12 +14,15 @@ private const val EXTRA_ANSWER_IS_TRUE = "com.example.geoquiz_2.answer_is_true"
 class CheatActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCheatBinding
+    private var answerIsTrue = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityCheatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/MainActivity.kt
+++ GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/MainActivity.kt
@@ -47,7 +47,8 @@ class MainActivity : AppCompatActivity() {
         }
         binding.cheatButton.setOnClickListener {
             //Start CheatActivity
-            val intent = Intent(this, CheatActivity::class.java)
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
             startActivity(intent)
         }
 
