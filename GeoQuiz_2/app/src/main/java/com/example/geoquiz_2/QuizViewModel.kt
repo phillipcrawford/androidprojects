bb8e6e7043cd8089b8ba04d08543e52f2302a87e
Phillip <phillip.crawford3@gmail.com>
2025-02-25 23:51:47 -0700

    isCheater

--- GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/QuizViewModel.kt
+++ GeoQuiz_2/app/src/main/java/com/example/geoquiz_2/QuizViewModel.kt
@@ -19,6 +19,10 @@ class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel(
         Question(R.string.question_asia, true)
     )
 
+    var isCheater: Boolean
+        get() = savedStateHandle.get(IS_CHEATER_KEY) ?: false
+        set(value) = savedStateHandle.set(IS_CHEATER_KEY, value)
+
     private var currentIndex: Int
         get() = savedStateHandle[CURRENT_INDEX_KEY] ?: 0
         set(value) = savedStateHandle.set(CURRENT_INDEX_KEY, value)
