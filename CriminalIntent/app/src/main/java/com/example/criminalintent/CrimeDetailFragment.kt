feb70d297d53080a22907d321f9dac06d3b9dc8f
Phillip <phillip.crawford3@gmail.com>
2025-02-27 22:16:47 -0700

    bind changes to the edittext xml to the model

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import com.example.criminalintent.databinding.FragmentCrimeDetailBinding
 import java.util.Date
@@ -33,5 +34,15 @@ class CrimeDetailFragment : Fragment() {
         binding =
             FragmentCrimeDetailBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+        }
     }
 }
