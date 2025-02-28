81a438fff0468ae187e62608f60f7557335f5d2d
Phillip <phillip.crawford3@gmail.com>
2025-02-27 21:26:35 -0700

    body

--- CriminalIntent/app/src/main/java/com/example/criminalintent/Crime.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/Crime.kt
@@ -1,3 +1,11 @@
 package com.example.criminalintent
 
-data class Crime()
+import java.util.Date
+import java.util.UUID
+
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean,
+)
