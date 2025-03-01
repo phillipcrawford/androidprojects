0028e8260c0e18c67a8a7519a41236e7aaaa1b94
Phillip <phillip.crawford3@gmail.com>
2025-02-28 23:23:42 -0700

    e

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -15,7 +15,7 @@ class CrimeDetailFragment : Fragment() {
     private var _binding: FragmentCrimeDetailBinding? = null
     private val binding
         get() = checkNotNull(_binding) {
-            "Cannot access binding because it is null. Is the view visabcdible?"
+            "Cannot access binding because it is null. Is the view visabcdeible?"
         }
 
     private lateinit var crime: Crime
