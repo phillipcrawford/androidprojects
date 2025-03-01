0f03f315edaa8d96f73c7bdb38b69699b921da6b
Phillip <phillip.crawford3@gmail.com>
2025-02-28 23:22:31 -0700

    change error message

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -15,7 +15,7 @@ class CrimeDetailFragment : Fragment() {
     private var _binding: FragmentCrimeDetailBinding? = null
     private val binding
         get() = checkNotNull(_binding) {
-            "Cannot access binding because it is null. Is the view visible?"
+            "Cannot access binding because it is null. Is the view visaible?"
         }
 
     private lateinit var crime: Crime
