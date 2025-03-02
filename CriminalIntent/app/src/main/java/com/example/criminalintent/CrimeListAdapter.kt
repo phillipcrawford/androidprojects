3cee0bbc1370c16b7e63a12d32a263cd3ed2b0a8
Phillip <phillip.crawford3@gmail.com>
2025-03-01 19:44:58 -0700

    adapter so recycler view can hold holders

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
@@ -1,10 +1,38 @@
 package com.example.criminalintent
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.criminalintent.databinding.ListItemCrimeBinding
+import java.util.UUID
 
-class CrimeListAdapter(
+class CrimeHolder(
     val binding: ListItemCrimeBinding
 ) : RecyclerView.ViewHolder(binding.root) {
-    
+
+}
+
+class CrimeListAdapter(
+    private val crimes: List<Crime>,
+    private val onCrimeClicked: (crimeId: UUID) -> Unit
+) : RecyclerView.Adapter<CrimeHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CrimeHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
+        return CrimeHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+        val crime = crimes[position]
+        holder.apply {
+            binding.crimeTitle.text = crime.title
+            binding.crimeDate.text = crime.date.toString()
+        }
+    }
+
+    override fun getItemCount() = crimes.size
 }
