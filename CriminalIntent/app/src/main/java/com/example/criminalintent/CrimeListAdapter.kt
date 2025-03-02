f213b7398167ed0f2726aef7d62ddf7dc092b964
Phillip <phillip.crawford3@gmail.com>
2025-03-01 19:39:51 -0700

    build a class that extends ViewHolder

---
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.criminalintent
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.criminalintent.databinding.ListItemCrimeBinding
+
+class CrimeListAdapter(
+    val binding: ListItemCrimeBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    
+}
