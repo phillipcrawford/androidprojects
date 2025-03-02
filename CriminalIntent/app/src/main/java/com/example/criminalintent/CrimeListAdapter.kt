402af28124a0ef7379626247a20670980651d75b
Phillip <phillip.crawford3@gmail.com>
2025-03-01 19:55:25 -0700

    set onclicklistener to make a toast

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
@@ -2,6 +2,7 @@ package com.example.criminalintent
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.criminalintent.databinding.ListItemCrimeBinding
 import java.util.UUID
@@ -12,6 +13,14 @@ class CrimeHolder(
     fun bind(crime: Crime) {
         binding.crimeTitle.text = crime.title
         binding.crimeDate.text = crime.date.toString()
+
+        binding.root.setOnClickListener {
+            Toast.makeText(
+                binding.root.context,
+                "${crime.title} clicked!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
 
