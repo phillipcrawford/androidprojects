1cd9bf73aebd31f34fb3b577f0af70213d8b0f67
Phillip <phillip.crawford3@gmail.com>
2025-03-03 21:29:29 -0700

    revert

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -12,7 +12,7 @@ class CrimeListViewModel : ViewModel() {
         for (i in 0 until 100) {
             val crime = Crime(
                 id = UUID.randomUUID(),
-                title = "Cabcdefghijrime #$i",
+                title = "Crime #$i",
                 date = Date(),
                 isSolved = i % 2 == 0
             )
