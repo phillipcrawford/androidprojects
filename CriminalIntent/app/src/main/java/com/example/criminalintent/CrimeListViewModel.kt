fc90ad536554f198bfd1f0534aa3e4533a0e1118
Phillip <phillip.crawford3@gmail.com>
2025-03-05 23:11:07 -0700

    imports, log statements call to delay for 5000 ms

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -1,17 +1,24 @@
 package com.example.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
+
+private const val TAG = "CrimeListViewModel"
 
 class CrimeListViewModel : ViewModel() {
 
     val crimes = mutableListOf<Crime>()
 
     init {
+        Log.d(TAG, "init starting")
         viewModelScope.launch {
+            Log.d(TAG, "coroutine launched")
+            delay(5000)
             for (i in 0 until 100) {
                 val crime = Crime(
                     id = UUID.randomUUID(),
