595c845e5ef6457ba97f2b9798b4b9cd8be7fce7
Phillip <phillip.crawford3@gmail.com>
2025-03-06 00:00:32 -0700

    version catalogue3

--- CriminalIntent/app/build.gradle.kts
+++ CriminalIntent/app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation(libs.androidx.recyclerview)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
