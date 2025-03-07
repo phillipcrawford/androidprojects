60561b7d31a24ca6b25fa8d6925da5d8ebaf5c9c
Phillip <phillip.crawford3@gmail.com>
2025-03-06 23:09:22 -0700

    plugins

--- CriminalIntent/app/build.gradle.kts
+++ CriminalIntent/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs")
 }
 
 android {
