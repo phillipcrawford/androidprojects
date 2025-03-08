1f074e34f95ea20c7822fa2de75684a70fc4f311
Phillip <phillip.crawford3@gmail.com>
2025-03-07 23:45:17 -0700

    room.TypeConverter

--- CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
@@ -2,6 +2,7 @@ package com.example.criminalintent.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.criminalintent.Crime
 
 @Database(entities = [ Crime::class ], version = 1)
