a0c53a7cbf688d72e6f402ff0e28b168e68428ae
Phillip <phillip.crawford3@gmail.com>
2025-03-07 23:32:36 -0700

    import crime model

--- CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
@@ -1,6 +1,7 @@
 package com.example.criminalintent.database
 
 import androidx.room.Database
+import com.example.criminalintent.Crime
 
 @Database(entities = [ Crime::class ], version = 1)
 abstract class CrimeDatabase {
