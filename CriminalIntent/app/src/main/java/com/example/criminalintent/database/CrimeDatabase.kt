fb5a93070d1dc2f812ddaffc0f3d338a5fdc3c7d
Phillip <phillip.crawford3@gmail.com>
2025-03-07 23:36:56 -0700

    import roomdatabase

--- CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
@@ -1,6 +1,7 @@
 package com.example.criminalintent.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.criminalintent.Crime
 
 @Database(entities = [ Crime::class ], version = 1)
