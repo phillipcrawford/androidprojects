d29d0b43dcc524abbffebdecd48a204d65d66b0d
Phillip <phillip.crawford3@gmail.com>
2025-03-07 23:44:50 -0700

    annotation

--- CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
@@ -5,5 +5,6 @@ import androidx.room.RoomDatabase
 import com.example.criminalintent.Crime
 
 @Database(entities = [ Crime::class ], version = 1)
+@TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
 }
