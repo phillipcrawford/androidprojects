85eada4c20c4c7d684792862165973859d2cd694
Phillip <phillip.crawford3@gmail.com>
2025-03-08 20:27:36 -0700

    tag and call to .build()

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeRepository.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Room
 import com.example.criminalintent.database.CrimeDatabase
 
+private const val DATABASE_NAME = "crime-database"
+
 class CrimeRepository private constructor(context: Context) {
 
     private val database: CrimeDatabase = Room
@@ -12,6 +14,7 @@ class CrimeRepository private constructor(context: Context) {
             CrimeDatabase::class.java,
             DATABASE_NAME
         )
+        .build()
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
