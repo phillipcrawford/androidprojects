af6b5bff5cfabb503d0e0c141081449101cdadbd
Phillip <phillip.crawford3@gmail.com>
2025-03-08 19:00:33 -0700

    body

--- CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDao.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDao.kt
@@ -2,8 +2,10 @@ package com.example.criminalintent.database
 
 import androidx.room.Dao
 import androidx.room.Query
+import com.example.criminalintent.Crime
 
 @Dao
 interface CrimeDao {
     @Query("SELECT * FROM crime")
+    suspend fun getCrimes(): List<Crime>
 }
