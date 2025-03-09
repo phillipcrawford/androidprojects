4e0b19cc662dbba5b02be2f4d93a32bdc47051b3
Phillip <phillip.crawford3@gmail.com>
2025-03-08 20:13:10 -0700

    setup repository properties

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeRepository.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -1,8 +1,17 @@
 package com.example.criminalintent
 
 import android.content.Context
+import androidx.room.Room
+import com.example.criminalintent.database.CrimeDatabase
 
 class CrimeRepository private constructor(context: Context) {
+
+    private val database: CrimeDatabase = Room
+        .databaseBuilder(
+            context.applicationContext,
+            CrimeDatabase::class.java,
+            DATABASE_NAME
+        )
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
