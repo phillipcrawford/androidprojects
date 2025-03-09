38bada5e5ba6ab1dc2d81638d32cee576830e058
Phillip <phillip.crawford3@gmail.com>
2025-03-08 20:46:48 -0700

    create a flow

--- CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDao.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDao.kt
@@ -3,11 +3,12 @@ package com.example.criminalintent.database
 import androidx.room.Dao
 import androidx.room.Query
 import com.example.criminalintent.Crime
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CrimeDao {
     @Query("SELECT * FROM crime")
-    suspend fun getCrimes(): List<Crime>
+    fun getCrimes(): Flow<List<Crime>>
 
     @Query("SELECT * FROM crime WHERE id=(:id)")
     suspend fun getCrime(id: String): Crime
