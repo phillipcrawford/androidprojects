88622187d0cf161f0f8e2401cae559553f6ce091
Phillip <phillip.crawford3@gmail.com>
2025-03-14 01:38:00 -0600

    update version in safe args, call to navigate

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.criminalintent.databinding.FragmentCrimeListBinding
 import kotlinx.coroutines.launch
@@ -42,7 +43,11 @@ class CrimeListFragment : Fragment() {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 crimeListViewModel.crimes.collect { crimes ->
                     binding.crimeRecyclerView.adapter =
-                        CrimeListAdapter(crimes)
+                        CrimeListAdapter(crimes) {
+                            findNavController().navigate(
+                                CrimeListFragmentDirections.showCrimeDetail()
+                            )
+                        }
                 }
             }
         }
--- CriminalIntent/build.gradle.kts
+++ CriminalIntent/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.8.8" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.8.9" apply false
 }
