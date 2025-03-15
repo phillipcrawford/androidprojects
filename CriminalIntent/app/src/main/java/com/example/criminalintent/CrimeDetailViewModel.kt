a9200851cf0ef61f3cb612503d66d2d58c61f68f
Phillip <phillip.crawford3@gmail.com>
2025-03-15 15:11:52 -0600

    13.26

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailViewModel.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailViewModel.kt
@@ -27,6 +27,11 @@ class CrimeDetailViewModel(crimeId: UUID) : ViewModel() {
             oldCrime?.let { onUpdate(it) }
         }
     }
+
+    override fun onCleared() {
+        super.onCleared()
+        crime.value?.let { crimeRepository.updateCrime(it) }
+    }
 }
 
 class CrimeDetailViewModelFactory(
--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeRepository.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -3,12 +3,18 @@ package com.example.criminalintent
 import android.content.Context
 import androidx.room.Room
 import com.example.criminalintent.database.CrimeDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import java.util.UUID
 
 private const val DATABASE_NAME = "crime-database"
 
-class CrimeRepository private constructor(context: Context) {
+class CrimeRepository private constructor(
+    context: Context,
+    private val coroutineScope: CoroutineScope = GlobalScope
+) {
 
     private val database: CrimeDatabase = Room
         .databaseBuilder(
@@ -23,8 +29,10 @@ class CrimeRepository private constructor(context: Context) {
 
     suspend fun getCrime(id: UUID): Crime = database.crimeDao().getCrime(id)
 
-    suspend fun updateCrime(crime: Crime) {
-        database.crimeDao().updateCrime(crime)
+    fun updateCrime(crime: Crime) {
+        coroutineScope.launch {
+            database.crimeDao().updateCrime(crime)
+        }
     }
 
     companion object {
