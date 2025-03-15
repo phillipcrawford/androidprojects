e23d1dfff6107cf4486d27cb8f43d014379e0b74
Phillip <phillip.crawford3@gmail.com>
2025-03-15 14:04:16 -0600

    13.18

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.example.criminalintent.databinding.FragmentCrimeDetailBinding
 import java.util.Date
@@ -21,6 +22,10 @@ class CrimeDetailFragment : Fragment() {
         }
 
     private val args: CrimeDetailFragmentArgs by navArgs()
+
+    private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
+        CrimeDetailViewModelFactory(args.crimeId)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
