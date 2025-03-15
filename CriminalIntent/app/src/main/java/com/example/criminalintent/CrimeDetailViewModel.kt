4853bc1b6f5f0be106f80e23a3f05684948b95cf
Phillip <phillip.crawford3@gmail.com>
2025-03-15 01:05:31 -0600

    body

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailViewModel.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.criminalintent
 
-class CrimeDetailViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class CrimeDetailViewModel : ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
+
+    private val _crime: MutableStateFlow<Crime?> = MutableStateFlow(null)
+    val crime: StateFlow<Crime?> = _crime.asStateFlow()
 }
