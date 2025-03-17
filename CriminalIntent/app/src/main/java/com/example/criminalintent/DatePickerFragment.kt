b02d46a402cb02c2b25d6ee116f9d3525c082537
Phillip <phillip.crawford3@gmail.com>
2025-03-16 20:40:19 -0600

    onCreateDialogue fu

--- CriminalIntent/app/src/main/java/com/example/criminalintent/DatePickerFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/DatePickerFragment.kt
@@ -1,8 +1,13 @@
 package com.example.criminalintent
 
+import android.app.Dialog
+import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 
 class DatePickerFragment : DialogFragment()  {
 
-    
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        
+        return super.onCreateDialog(savedInstanceState)
+    }
 }
