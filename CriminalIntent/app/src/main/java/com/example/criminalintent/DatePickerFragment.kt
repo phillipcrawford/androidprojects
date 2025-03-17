e8d94fd00343ae20ba1d54e7b2c34e6f08644e13
Phillip <phillip.crawford3@gmail.com>
2025-03-16 22:23:39 -0600

    send back the date

--- CriminalIntent/app/src/main/java/com/example/criminalintent/DatePickerFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/DatePickerFragment.kt
@@ -4,14 +4,28 @@ import android.app.DatePickerDialog
 import android.app.Dialog
 import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.DatePicker
+import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.navArgs
+import java.util.GregorianCalendar
 
 class DatePickerFragment : DialogFragment()  {
 
     private val args: DatePickerFragmentArgs by navArgs()
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dateListener = DatePickerDialog.OnDateSetListener{
+                _: DatePicker, year: Int, month: Int, day: Int ->
+
+            val resultDate = GregorianCalendar(year, month, day).time
+
+            setFragmentResult(
+                REQUEST_KEY_DATE,
+                bundleOf(BUNDLE_KEY_DATE to resultDate)
+            )
+        }
         val calendar = Calendar.getInstance()
         calendar.time = args.crimeDate
         val initialYear = calendar.get(Calendar.YEAR)
@@ -20,7 +34,7 @@ class DatePickerFragment : DialogFragment()  {
 
         return DatePickerDialog(
             requireContext(),
-            null,
+            dateListener,
             initialYear,
             initialMonth,
             initialDay
@@ -29,5 +43,6 @@ class DatePickerFragment : DialogFragment()  {
 
     companion object {
         const val REQUEST_KEY_DATE = "REQUEST_KEY_DATE"
+        const val BUNDLE_KEY_DATE = "BUNDLE_KEY_DATE"
     }
 }
