39ca5d98479fc86b9ab1b15330306d666e4f1f09
Phillip <phillip.crawford3@gmail.com>
2025-03-18 23:50:29 -0600

    provide migration to room

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeRepository.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -3,6 +3,7 @@ package com.example.criminalintent
 import android.content.Context
 import androidx.room.Room
 import com.example.criminalintent.database.CrimeDatabase
+import com.example.criminalintent.database.migration_1_2
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
@@ -22,6 +23,7 @@ class CrimeRepository private constructor(
             CrimeDatabase::class.java,
             DATABASE_NAME
         )
+        .addMigrations(migration_1_2)
         .createFromAsset(DATABASE_NAME)
         .build()
 
