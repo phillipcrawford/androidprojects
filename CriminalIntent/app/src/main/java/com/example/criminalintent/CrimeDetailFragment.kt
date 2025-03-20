c3198da867f692aa59d78b1805fc70eab1d58876
Phillip <phillip.crawford3@gmail.com>
2025-03-19 22:47:24 -0600

    b

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -18,7 +18,7 @@ import com.example.criminalintent.databinding.FragmentCrimeDetailBinding
 import kotlinx.coroutines.launch
 import java.util.Date
 
-private const val DATE_FORMAT = "EEE, MMM, dda"
+private const val DATE_FORMAT = "EEE, MMM, ddab"
 
 class CrimeDetailFragment : Fragment() {
 
