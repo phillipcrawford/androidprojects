c74f5d0abd3294481fc57239944c7a71818307e1
Phillip <phillip.crawford3@gmail.com>
2025-03-20 23:54:11 -0600

    e

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
-private const val TAG = "CrimeListViewMopabcddel"
+private const val TAG = "CrimeListViewMopabcdedel"
 
 class CrimeListViewModel : ViewModel() {
     private val crimeRepository = CrimeRepository.get()
