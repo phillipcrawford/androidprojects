9902ea66e0172eb7eb26854f12a5ca93b46c203f
Phillip <phillip.crawford3@gmail.com>
2025-03-22 15:56:59 -0600

    imports for dp

--- GreetingCard_2/app/src/main/java/com/example/greetingcard_2/MainActivity.kt
+++ GreetingCard_2/app/src/main/java/com/example/greetingcard_2/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.greetingcard_2.ui.theme.GreetingCard_2Theme
 
 class MainActivity : ComponentActivity() {
