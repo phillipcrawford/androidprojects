72f25d026057158b8f242bf36661191f99ee2bbc
Phillip <phillip.crawford3@gmail.com>
2025-03-22 15:09:55 -0600

    change surface to CCyan

--- GreetingCard_2/app/src/main/java/com/example/greetingcard_2/MainActivity.kt
+++ GreetingCard_2/app/src/main/java/com/example/greetingcard_2/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.greetingcard_2.ui.theme.GreetingCard_2Theme
 
@@ -32,16 +34,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hi my name is $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GreetingCard_2Theme {
-        Greeting("Android")
+        Greeting("Phillip")
     }
 }
