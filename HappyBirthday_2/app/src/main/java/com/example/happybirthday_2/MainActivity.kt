346ff96240afc0ad70b696ad08314ba503df94e2
Phillip <phillip.crawford3@gmail.com>
2025-03-22 19:19:24 -0600

    get fillMaxSize() right with Scaffold

--- HappyBirthday_2/app/src/main/java/com/example/happybirthday_2/MainActivity.kt
+++ HappyBirthday_2/app/src/main/java/com/example/happybirthday_2/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,11 +28,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthday_2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                Scaffold { innerPadding ->
                     GreetingText(
                         "Happy Birthday Sam!",
                         "From Emma",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     )
                 }
             }
