62ac359b1340e600b54fb9f87630be2e67cf8bef
Phillip <phillip.crawford3@gmail.com>
2025-03-22 18:47:58 -0600

    add padding to column's modifier, Arrangement at center

--- HappyBirthday_2/app/src/main/java/com/example/happybirthday_2/MainActivity.kt
+++ HappyBirthday_2/app/src/main/java/com/example/happybirthday_2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday_2.ui.theme.HappyBirthday_2Theme
 
@@ -37,7 +39,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column (modifier = modifier) {
+    Column (verticalArrangement = Arrangement.Center, modifier = modifier.padding(8.dp)) {
         Text(
             text = message,
             fontSize = 100.sp,
