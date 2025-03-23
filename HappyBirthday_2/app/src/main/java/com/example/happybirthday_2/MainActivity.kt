89d88177bd4b18ae74dd42fbe09ec9e1ce865cda
Phillip <phillip.crawford3@gmail.com>
2025-03-22 19:53:20 -0600

    GreetingImage composable

--- HappyBirthday_2/app/src/main/java/com/example/happybirthday_2/MainActivity.kt
+++ HappyBirthday_2/app/src/main/java/com/example/happybirthday_2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -65,10 +67,19 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty1)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthday_2Theme {
-        GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+        GreetingImage(message = "Happy Birthday Sam!", from = "From Emma")
     }
 }
