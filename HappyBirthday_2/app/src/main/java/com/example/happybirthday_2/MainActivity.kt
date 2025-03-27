7ec4f91d379804f10b58340f3645a19ff4662a3b
Phillip <phillip.crawford3@gmail.com>
2025-03-26 23:06:05 -0600

    string resource

--- HappyBirthday_2/app/src/main/java/com/example/happybirthday_2/MainActivity.kt
+++ HappyBirthday_2/app/src/main/java/com/example/happybirthday_2/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,7 +36,7 @@ class MainActivity : ComponentActivity() {
 
                 Scaffold { innerPadding ->
                     GreetingImage(
-                        "Happy Birthday Sam!",
+                        message = stringResource(R.string.happy_birthday_text),
                         "From Emma",
                         modifier = Modifier
                             .fillMaxSize()
