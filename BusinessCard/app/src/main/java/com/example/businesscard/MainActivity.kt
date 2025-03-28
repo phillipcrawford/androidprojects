a54fd54bd9435a529e79cb5787cadaba97c85fa0
Phillip <phillip.crawford3@gmail.com>
2025-03-27 23:50:52 -0600

    change string g

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -145,7 +145,7 @@ fun ContactInfo(modifier: Modifier = Modifier) {
             Text(
                 modifier = Modifier
                     .padding(8.dp),
-                text = " @philsweralipcr86",
+                text = " @philsweralippcr86",
                 fontSize = 20.sp
             )
         }
