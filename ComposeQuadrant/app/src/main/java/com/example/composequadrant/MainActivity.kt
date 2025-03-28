55cda7273b85c67518967f0430958a2b72653fde
Phillip <phillip.crawford3@gmail.com>
2025-03-27 21:32:50 -0600

    update gradle for 3 projects

--- ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -36,8 +36,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeQuadrantTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize()
-                    //color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     ComposeQuadrantApp()
                 }
