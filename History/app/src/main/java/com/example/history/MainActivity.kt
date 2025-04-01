565e8514c630cc590607885e16e474b3dda5013c
Phillip <phillip.crawford3@gmail.com>
2025-03-31 23:25:09 -0600

    High Protein

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -131,7 +131,7 @@ fun TwoBySixteen(name: String, modifier: Modifier = Modifier) {
                 modifier = Modifier.weight(1f)
             )
             Text(
-                text = "Hello $name!",
+                text = "High Protein",
                 modifier = Modifier.weight(1f)
             )
         }
