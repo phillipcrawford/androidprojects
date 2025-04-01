cd6d19182988a760092eb7e8b321de22097a5a95
Phillip <phillip.crawford3@gmail.com>
2025-03-31 23:18:17 -0600

    button1

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.history
 
 import android.os.Bundle
+import androidx.compose.material3.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,11 +9,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.history.ui.theme.HistoryTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,12 +39,19 @@ class MainActivity : ComponentActivity() {
 fun TwoBySixteen(name: String, modifier: Modifier = Modifier) {
     Column {
         Row(Modifier.weight(1f)) {
-            Text(
-                text = "Vegetarian",
+            Button(
+                onClick = {
+
+                }, shape = RoundedCornerShape(40.dp),
                 modifier = Modifier.weight(1f)
-            )
+            ) {
+                Text(
+                    text = "Vegetarian",
+                    modifier = Modifier.weight(1f)
+                )
+            }
             Text(
-                text = "Vegan",
+                text = "Pescetarian",
                 modifier = Modifier.weight(1f)
             )
         }
