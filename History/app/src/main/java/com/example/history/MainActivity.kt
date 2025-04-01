be3e1c3c38a6c327b29cd15807bd7c18e4d90de1
Phillip <phillip.crawford3@gmail.com>
2025-03-31 23:24:15 -0600

    Seafood

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -121,7 +121,7 @@ fun TwoBySixteen(name: String, modifier: Modifier = Modifier) {
                 modifier = Modifier.weight(1f)
             )
             Text(
-                text = "Hello $name!",
+                text = "Seafood",
                 modifier = Modifier.weight(1f)
             )
         }
