0d80eb4d5249d1cf49e5e3c03d63e7102252d193
Phillip <phillip.crawford3@gmail.com>
2025-04-02 23:58:48 -0600

    Halal

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -177,7 +177,7 @@ fun TwoBySixteen(name: String, modifier: Modifier = Modifier) {
                 modifier = Modifier.weight(1f)
             ) {
                 Text(
-                    text = "Sweet",
+                    text = "Halal",
                     modifier = Modifier.weight(1f)
                 )
             }
