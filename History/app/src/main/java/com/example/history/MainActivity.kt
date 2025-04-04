7c3cca3193db37531622e2b5c3c724efa027a437
Phillip <phillip.crawford3@gmail.com>
2025-04-03 22:58:42 -0600

    no shellfish

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -369,7 +369,7 @@ fun TwoBySixteen(name: String, modifier: Modifier = Modifier) {
                 modifier = Modifier.weight(1f)
             ) {
                 Text(
-                    text = "No Eggs",
+                    text = "No Shellfish",
                     modifier = Modifier.weight(1f)
                 )
             }
