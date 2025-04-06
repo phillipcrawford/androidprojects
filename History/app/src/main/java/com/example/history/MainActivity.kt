0a7d17053fcd74db97cea90d7497b7c201fadeb6
Phillip <phillip.crawford3@gmail.com>
2025-04-05 23:11:14 -0600

    columnswqrtponyoppvbmx

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DietprefsStartPage (name: String, modifier: Modifier = Modifier) {
     Column (modifier = Modifier) {}
-    Text(text = "Preferwrqspternyopnvpbmcoes", modifier = modifier)
+    Text(text = "Preferwrqspternyopnxvpbmcoes", modifier = modifier)
 }
 
 @Composable
