530a69d4b47f02fe601e27d70ee2125abdee958b
Phillip <phillip.crawford3@gmail.com>
2025-04-06 23:57:32 -0600

    typobc

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DietprefsStartPage (name: String, modifier: Modifier = Modifier) {
     Column (modifier = Modifier) {
-        Header(name = "Prefereabnces", modifier = modifier)
+        Header(name = "Prefereabcnces", modifier = modifier)
         TwoBySixteen(name = name, modifier = modifier)
     }
 }
