2080d9db6a1a0ac517acfae7c2e9fc2dc28f3f1e
Phillip <phillip.crawford3@gmail.com>
2025-04-07 23:58:50 -0600

    bas

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -62,7 +62,7 @@ fun Header(name: String, modifier: Modifier = Modifier) {
     ) {
         Row(modifier.weight(1f)) {
             Text(
-                text = "$name!Ba",
+                text = "$name!Bsa",
                 modifier = modifier,
             )
         }
