dece8f367f1940609c37545e07ef59dcff0b9079
Phillip <phillip.crawford3@gmail.com>
2025-04-09 23:59:18 -0600

    abcdefghijklmnop

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -75,7 +75,7 @@ fun TwoBySixteen(name: String, modifier: Modifier = Modifier) {
             verticalAlignment = Alignment.CenterVertically
         ){
             Text(
-                text = "Preferencbacdefghijklmnoes",
+                text = "Preferencbacdefghijklmnopes",
                 modifier = Modifier.weight(1f),
                 textAlign = TextAlign.Center
             )
