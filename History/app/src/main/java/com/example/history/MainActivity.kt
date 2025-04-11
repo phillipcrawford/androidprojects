610901486871a791a697fc299d3de03c1df2ed91
Phillip <phillip.crawford3@gmail.com>
2025-04-10 23:58:48 -0600

    heademodrabcdef

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -59,7 +59,7 @@ fun Header(name: String, modifier: Modifier = Modifier) {
     ) {
         Row(modifier.weight(12f)) {
             Text(
-                text = "$nameaedcb!",
+                text = "$nameaedfcb!",
                 modifier = modifier,
             )
         }
