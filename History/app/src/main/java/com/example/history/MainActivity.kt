5ed18d088237a46e2b9879946e08be24f33def2d
Phillip <phillip.crawford3@gmail.com>
2025-04-12 23:43:42 -0600

    get emulator running at this workstationcd

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
             HistoryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     DietprefsStartPage(
-                        name = "Andabrboccid",
+                        name = "Andabrbocdcid",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
