aa585f795f287177d0977369eed4ac2ca32050a2
Phillip <phillip.crawford3@gmail.com>
2025-04-12 23:54:23 -0600

    modify stringv

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
             HistoryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     DietprefsStartPage(
-                        name = "Androidq",
+                        name = "Androidv",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
