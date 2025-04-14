b796c3dc06d86e9d40de3f1c1d4a4e7974559111
Phillip Crawford <phillip.crawford3@gmail.com>
2025-04-14 11:35:41 -0500

    q

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
             HistoryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     DietprefsStartPage(
-                        name = "Androidebfdgikmjnohplca!",
+                        name = "Androidebfdgikmjnoqhplca!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
