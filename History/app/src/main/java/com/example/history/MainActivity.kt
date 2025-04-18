c64aca47e9382893680d6d83880283b121903e6c
phillipcrawford <phillip.crawford3@gmail.com>
2025-04-17 23:56:44 -0600

    Androied!

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
             HistoryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     DietprefsStartPage(
-                        name = "Android!",
+                        name = "Androied!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
