418cf47cf24eff89b3e1c3740b405d9f3ba92005
phillipcrawford <phillip.crawford3@gmail.com>
2025-04-19 23:56:04 -0600

    j

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -69,7 +69,7 @@ fun DietprefsStartPage(name: String, modifier: Modifier = Modifier) {
                 colors = ButtonDefaults.buttonColors(containerColor = BlackOne)
             ) {
                 Text(
-                    text = "Preferenqces",
+                    text = "Preferenqjces",
                     modifier = Modifier.weight(1f)
                 )
             }
