cd2bb7696ebdadd9d0a7afc240b6e8b092d2990c
phillipcrawford <phillip.crawford3@gmail.com>
2025-04-23 13:56:05 -0600

    navHost

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -29,6 +29,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.history.ui.theme.BlackOne
 import com.example.history.ui.theme.BlackTwo
 import com.example.history.ui.theme.DOrange
@@ -43,7 +45,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HistoryTheme {
-                val navController = remember
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "home") {
+
+                }
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     DietprefsStartPage(
                         name = "Android!",
