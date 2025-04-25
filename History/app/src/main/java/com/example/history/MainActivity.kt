6b7d0fa46d7e45b1e90d27000f312dd37ac653cc
phillipcrawford <phillip.crawford3@gmail.com>
2025-04-24 23:55:49 -0600

    abdcefghij

--- History/app/src/main/java/com/example/history/MainActivity.kt
+++ History/app/src/main/java/com/example/history/MainActivity.kt
@@ -50,7 +50,7 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = "home") {
 //
-                    composable(route = "loabccgefghidin"){
+                    composable(route = "loabccgefghijdin"){
 
                     }
                 }
