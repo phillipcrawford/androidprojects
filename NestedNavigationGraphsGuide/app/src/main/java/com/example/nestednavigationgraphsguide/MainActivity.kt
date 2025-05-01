80476695d117a63d2dbde33b251acb9094dcb9c3
phillipcrawford <phillip.crawford3@gmail.com>
2025-04-30 20:45:33 -0600

    i'

--- NestedNavigationGraphsGuide/app/src/main/java/com/example/nestednavigationgraphsguide/MainActivity.kt
+++ NestedNavigationGraphsGuide/app/src/main/java/com/example/nestednavigationgraphsguide/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : ComponentActivity() {
                         startDestination = "login",
                         route = "auth"
                     ) {
-                        composable(route = "h") {
+                        composable(route = "logabcedifgin") {
                             val viewModel = it.sharedViewModel<SampleViewModel>(navController)
                         }
                         composable(route = "register") {
