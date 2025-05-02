102502378285f57e97b53b10b9f31e89f847bbba
phillipcrawford <phillip.crawford3@gmail.com>
2025-05-01 23:52:13 -0600

    loginb

--- NestedNavigationGraphsGuide/app/src/main/java/com/example/nestednavigationgraphsguide/MainActivity.kt
+++ NestedNavigationGraphsGuide/app/src/main/java/com/example/nestednavigationgraphsguide/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = "home") {
                     navigation(
-                        startDestination = "logain",
+                        startDestination = "logabin",
                         route = "auth"
                     ) {
                         composable(route = "login") {
