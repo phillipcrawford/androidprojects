3d57652b1760d36f14cb2967b22d882aeff03227
phillipcrawford <phillip.crawford3@gmail.com>
2025-05-21 13:12:31 -0600

    revert to boilerplate

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/MainActivity.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -3,32 +3,45 @@ package com.example.helloworldapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.helloworldapp.ui.theme.HelloWorldAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             HelloWorldAppTheme {
-                HelloWorldText()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
 }
 
 @Composable
-fun HelloWorldText() {
-    Text(text = "Hello, World!")
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun GreetingPreview() {
     HelloWorldAppTheme {
-        HelloWorldText()
+        Greeting("Android")
     }
 }
