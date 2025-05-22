85b29126d032217b35ed51a397249885b687dd05
phillipcrawford <phillip.crawford3@gmail.com>
2025-05-21 23:59:42 -0600

    b

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/MainActivity.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             HelloWorldAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "a",
+                        name = "b",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
