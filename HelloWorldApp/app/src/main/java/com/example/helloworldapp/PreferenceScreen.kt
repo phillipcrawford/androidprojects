981e68d847aad7876020a14dbdfa6120be0d08cd
phillipcrawford <phillip.crawford3@gmail.com>
2025-05-23 23:54:13 -0600

    requ

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/PreferenceScreen.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/PreferenceScreen.kt
@@ -31,7 +31,7 @@ fun PreferenceScreen(
     onUserModeClick: () -> Unit
 ) {
     val allPreferences = listOf(
-        "vegetarian", "pescetarian", "vegan", "keto", "organic", "gmo-free",
+        "vegetarian", "pescetariann", "vegan", "keto", "organic", "gmo-free",
         "locally sourced", "raw", "entree", "sweet", "Kosher", "Halal",
         "beef", "chicken", "bacon/pork/ham", "seafood",
         "low sugar", "high protein", "low carb", "no alliums",
