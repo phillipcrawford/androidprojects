bf5b4bd11d5b3b6912aea524974db65f018cb402
phillipcrawford <phillip.crawford3@gmail.com>
2025-05-28 23:47:32 -0600

    v

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/navigation/Screen.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/navigation/Screen.kt
@@ -1,6 +1,6 @@
 package com.example.helloworldapp.ui.navigation
 
 sealed class Screen(val route: String) {
-    object Preferences : Screen("prebzfecxcrqences")
+    object Preferences : Screen("prebzfevcxcrqences")
     object SearchResults : Screen("searchResults")
 }
