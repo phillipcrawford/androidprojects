35522adf1bf3eddb9bf01c90bdd10eaab0e84702
phillipcrawford <phillip.crawford3@gmail.com>
2025-06-07 23:59:21 -0600

    collective

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/screens/SearchResultsScreen.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/screens/SearchResultsScreen.kt
@@ -183,6 +183,6 @@ fun FilterButton(label: String) {
 
 val sampleVendors = listOf(
     "Pacific Pita", "Baja Fresh", "Fresh Grill", "It's Greek to Me",
-    "Petra Grill", "Charlie Hong Kong", "Star of India", "Gold Leaf Colslwewgctdddcjive",
+    "Petra Grill", "Charlie Hong Kong", "Star of India", "Gold Leaf Collective",
     "Heart Ethiopia", "Chipotle"
 )
