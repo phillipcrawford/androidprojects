1246a0c01bae16b30c359d6a772daf34c44327c0
phillipcrawford <phillip.crawford3@gmail.com>
2025-06-17 23:56:30 -0600

    4dp

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/screens/PreferenceScreen.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/screens/PreferenceScreen.kt
@@ -62,7 +62,7 @@ fun PreferenceScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .background(dietprefsGrey)
-                    .padding(48.dp),
+                    .padding(4.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Button(
