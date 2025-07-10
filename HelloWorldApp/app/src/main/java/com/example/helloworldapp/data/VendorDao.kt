9665ab0d43b3dffed5607e31d71758148804f9e3
phillipcrawford <phillip.crawford3@gmail.com>
2025-07-09 20:14:21 -0600

    3 other data files

---
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/data/VendorDao.kt
@@ -0,0 +1,20 @@
+import androidx.room.*
+
+@Dao
+interface VendorDao {
+    @Transaction
+    @Query("SELECT * FROM vendors")
+    suspend fun getVendorsWithItems(): List<VendorWithItems>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertVendors(vendors: List<VendorEntity>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItems(items: List<ItemEntity>)
+
+    @Query("DELETE FROM vendors")
+    suspend fun clearVendors()
+
+    @Query("DELETE FROM items")
+    suspend fun clearItems()
+}
