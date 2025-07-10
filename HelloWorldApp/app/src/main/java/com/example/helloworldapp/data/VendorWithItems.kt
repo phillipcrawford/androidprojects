a115c5d6cc142fedc89cc13aa99c279e654b6a50
phillipcrawford <phillip.crawford3@gmail.com>
2025-07-09 22:32:28 -0600

    comments

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/data/VendorWithItems.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/data/VendorWithItems.kt
@@ -1,11 +1,11 @@
-import androidx.room.Embedded
-import androidx.room.Relation
-
-data class VendorWithItems(
-    @Embedded val vendor: VendorEntity,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "vendorId"
-    )
-    val items: List<ItemEntity>
-)
+//import androidx.room.Embedded
+//import androidx.room.Relation
+//
+//data class VendorWithItems(
+//    @Embedded val vendor: VendorEntity,
+//    @Relation(
+//        parentColumn = "id",
+//        entityColumn = "vendorId"
+//    )
+//    val items: List<ItemEntity>
+//)
