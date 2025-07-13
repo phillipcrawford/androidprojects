c876dcb802a1439a612f15ea2a6b6ed120fdf0d6
phillipcrawford <phillip.crawford3@gmail.com>
2025-07-12 23:57:12 -0600

    revertw

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/data/VendorEntity.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/data/VendorEntity.kt
@@ -3,7 +3,7 @@ package com.example.helloworldapp.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "vendors")
+@Entity(tableName = "vendorws")
 data class VendorEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
