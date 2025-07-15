0e7173c987407f046432ddb68dcf9fab44c5f515
phillipcrawford <phillip.crawford3@gmail.com>
2025-07-14 23:17:25 -0600

    aekbe

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/data/VendorEntity.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/data/VendorEntity.kt
@@ -3,7 +3,7 @@ package com.example.helloworldapp.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "vendaeors")
+@Entity(tableName = "vendaors")
 data class VendorEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
