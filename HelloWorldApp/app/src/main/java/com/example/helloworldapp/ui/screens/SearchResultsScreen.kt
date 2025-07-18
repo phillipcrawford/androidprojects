2c1a5795990af4886321361be342891b70941c58
phillipcrawford <phillip.crawford3@gmail.com>
2025-07-17 23:57:55 -0600

    we

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/screens/SearchResultsScreen.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/screens/SearchResultsScreen.kt
@@ -142,7 +142,7 @@ fun SearchResultsTopBar(
         ) {
             Icon(
                 imageVector = Icons.Default.ArrowBack,
-                contentDescription = "Bawsck",
+                contentDescription = "Bawck",
                 tint = Color.White
             )
         }
