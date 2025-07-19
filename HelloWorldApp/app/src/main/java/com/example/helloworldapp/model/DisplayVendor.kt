99c2f7ff76317891c3692c106f0833ebbe4268c9
phillipcrawford <phillip.crawford3@gmail.com>
2025-07-19 12:22:37 -0600

    model for frontend

---
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/model/DisplayVendor.kt
@@ -0,0 +1,8 @@
+package com.example.helloworldapp.model
+
+data class DisplayVendor(
+    val vendorName: String,
+    val user1Count: Int,
+    val user2Count: Int,
+    val distanceMiles: Float
+)
