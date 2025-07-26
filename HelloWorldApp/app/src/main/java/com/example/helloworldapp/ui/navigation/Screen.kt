2206b34d657beb28999e368cf9d05243c80cc286
phillipcrawford <phillip.crawford3@gmail.com>
2025-07-26 10:51:00 -0600

    Settings and vendorDetail routes

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/navigation/Screen.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/navigation/Screen.kt
@@ -3,4 +3,6 @@ package com.example.helloworldapp.ui.navigation
 sealed class Screen(val route: String) {
     object Preferences : Screen("preferences")
     object SearchResults : Screen("searchResults")
+    object Settings : Screen("SettingsScreen")
+    object VendorDetail : Screen("VendorDetailScreen")
 }
