7e7c33987eb7fb80e3ea864c8ed2789bfd2d9ac1
phillipcrawford <phillip.crawford3@gmail.com>
2025-08-01 23:58:13 -0600

    eio

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/screens/SearchResultsScreen.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/screens/SearchResultsScreen.kt
@@ -152,7 +152,7 @@ fun SearchResultsScreen(
                     contentAlignment = Alignment.Center
                 ) {
                     SortableHeader(
-                        text = "Dpiest",
+                        text = "Diest",
                         column = SortColumn.DISTANCE,
                         currentSortState = sortState,
                         onClick = { /*sharedViewModel.updateSortState(SortColumn.DISTANCE) */ },
