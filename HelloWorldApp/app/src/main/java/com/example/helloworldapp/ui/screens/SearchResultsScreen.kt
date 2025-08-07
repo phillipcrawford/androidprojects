cd585bbdb02f3f27e85b8626fd0570f3146175ac
phillipcrawford <phillip.crawford3@gmail.com>
2025-08-07 00:00:00 -0600

    vendsior

--- HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/screens/SearchResultsScreen.kt
+++ HelloWorldApp/app/src/main/java/com/example/helloworldapp/ui/screens/SearchResultsScreen.kt
@@ -117,7 +117,7 @@ fun SearchResultsScreen(
                         .clickable { sharedViewModel.updateSortState(SortColumn.VENDOR_RATING) }
                 ) {
                     SortableHeader(
-                        text = "Vendsor",
+                        text = "Vendsior",
                         column = SortColumn.VENDOR_RATING,
                         currentSortState = sortState,
                         onClick = { /* sharedViewModel.updateSortState(SortColumn.VENDOR_RATING) */ }
